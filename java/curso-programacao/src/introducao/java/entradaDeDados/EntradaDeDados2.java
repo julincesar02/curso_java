a50e4bd2294a46892cc419d6eb1af10954382c25
julincesar02 <juliogoiabinha060@gmail.com>
2025-08-10 20:01:45 -0300

    Entrada de Dados aula 02

--- java/curso-programacao/src/introducao/java/entradaDeDados/EntradaDeDados2.java
+++ java/curso-programacao/src/introducao/java/entradaDeDados/EntradaDeDados2.java
@@ -4,17 +4,22 @@ import java.util.Scanner;
 
 public class EntradaDeDados2 {
     public static void main(String[] args) {
-        // Ler varios elementos até quebrar linha
+        // aprendendo a ler varias entradas de dados até a quebra de linha
         Scanner sc = new Scanner(System.in);
-        // OBS: se colocamos um valor de tipo primitivo como int , double ect... , devemos colocar um proximo netxline vazio
+        // OBS: se colocamos um valor de tipo primitivo como int , double ect... , devemos colocar uma proxima linha vazia
         String s1 , s2 , s3;
+        double d ;
+        d = sc.nextDouble();
+        sc.nextLine();
         s1 = sc.nextLine();
         s2 = sc.nextLine();
         s3 = sc.nextLine();
+
         System.out.println("Dados guardados: ");
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
+        System.out.println(d);
         sc.close();
     }
 }
