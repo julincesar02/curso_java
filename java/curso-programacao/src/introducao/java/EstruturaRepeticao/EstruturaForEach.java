cea784325c9d6ffd8a8d120b7bfe1ec656ca4f81
julincesar02 <juliogoiabinha060@gmail.com>
2025-08-10 20:43:02 -0300

    Estrutura For I aula 09

---
+++ java/curso-programacao/src/introducao/java/EstruturaRepeticao/EstruturaForEach.java
@@ -0,0 +1,4 @@
+package introducao.java.EstruturaRepeticao;
+
+public class EstruturaForEach {
+}
--- java/curso-programacao/src/introducao/java/EstruturaRepeticao/Estruturafor2.java
+++ java/curso-programacao/src/introducao/java/EstruturaRepeticao/Estruturafor2.java
@@ -1,13 +1,21 @@
 package introducao.java.EstruturaRepeticao;
 
+import java.util.Scanner;
+
 public class Estruturafor2 {
     public static void main(String[] args) {
-        int valorMax = 20;
-        for (int i = 0; i <=valorMax; i++) {
-            if (valorMax > 25){
-                break;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Quantos números deseja: ");
+        int number = input.nextInt();
+        for (int i = 0; i < number + 1; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " É Par");
+                System.out.println();
             }
-            System.out.println(i);
+            if (i % 2 == 1) {
+                System.out.println(i + " É Impar");
+                System.out.println();
+            }
         }
     }
 }
